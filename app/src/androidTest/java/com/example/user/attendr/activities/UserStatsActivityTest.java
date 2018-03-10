8e4f59adb917a7c293648bc9423b6815fd5ac619
Fanner487 <eamontang1@hotmail.com>
2018-03-10 21:47:26 +0000

    Added more tests

--- app/src/androidTest/java/com/example/user/attendr/activities/UserStatsActivityTest.java
+++ app/src/androidTest/java/com/example/user/attendr/activities/UserStatsActivityTest.java
@@ -1,6 +1,24 @@
 package com.example.user.attendr.activities;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ImageView;
+import android.widget.ScrollView;
+import android.widget.TextClock;
+import android.widget.TextView;
+
+import com.example.user.attendr.R;
+import com.example.user.attendr.constants.DbConstants;
+import com.github.lzyzsd.circleprogress.DonutProgress;
+
 import org.junit.Rule;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertNotNull;
 
 /**
  * Created by Eamon on 10/03/2018.
@@ -9,5 +27,43 @@ import org.junit.Rule;
 public class UserStatsActivityTest {
 
     @Rule
-    public
+    public ActivityTestRule<UserStatsActivity> rule  = new ActivityTestRule<UserStatsActivity>(UserStatsActivity.class){
+        @Override
+        protected Intent getActivityIntent() {
+            Context targetContext = InstrumentationRegistry.getInstrumentation()
+                    .getTargetContext();
+            Intent result = new Intent(targetContext, UserStatsActivity.class);
+
+            Bundle bundle = new Bundle();
+            bundle.putString(DbConstants.GROUP_KEY_ROW_USERNAME, "test");
+//            result.putExtra(DbConstants.EVENT_KEY_EVENT_ID, 1);
+            result.putExtras(bundle);
+            return result;
+        }
+    };
+
+    @Test
+    public void testTextViewExists(){
+        UserStatsActivity activity = rule.getActivity();
+
+        TextView tvUsername = activity.findViewById(R.id.tvUsername);
+        TextView tvEventsAttended = activity.findViewById(R.id.tvEventsAttended);
+        TextView tvAttended = activity.findViewById(R.id.tvAttended);
+        TextView tvNotAttended = activity.findViewById(R.id.tvNotAttended);
+        RecyclerView rvAttended = activity.findViewById(R.id.rvAttended);
+        RecyclerView rvNotAttended = activity.findViewById(R.id.rvNotAttended);
+        DonutProgress donutProgress = activity.findViewById(R.id.donut_progress);
+        ScrollView scrollView = activity.findViewById(R.id.scrollView);
+
+        assertNotNull(tvUsername);
+        assertNotNull(tvEventsAttended);
+        assertNotNull(tvAttended);
+        assertNotNull(tvNotAttended);
+        assertNotNull(rvAttended);
+        assertNotNull(rvNotAttended);
+        assertNotNull(donutProgress);
+        assertNotNull(scrollView);
+
+    }
+
 }
---
+++ app/src/androidTest/java/com/example/user/attendr/activities/ViewGroupsActivityTest.java
@@ -0,0 +1,65 @@
+package com.example.user.attendr.activities;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import com.example.user.attendr.R;
+import com.example.user.attendr.constants.DbConstants;
+import com.github.lzyzsd.circleprogress.DonutProgress;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+
+/**
+ * Created by Eamon on 10/03/2018.
+ */
+
+public class ViewGroupsActivityTest {
+
+    @Rule
+    public ActivityTestRule<ViewGroupsActivity> rule  = new ActivityTestRule<ViewGroupsActivity>(ViewGroupsActivity.class){
+        @Override
+        protected Intent getActivityIntent() {
+            Context targetContext = InstrumentationRegistry.getInstrumentation()
+                    .getTargetContext();
+            Intent result = new Intent(targetContext, ViewGroupsActivity.class);
+
+            Bundle bundle = new Bundle();
+            bundle.putString(DbConstants.GROUP_KEY_ROW_USERNAME, "test");
+//            result.putExtra(DbConstants.EVENT_KEY_EVENT_ID, 1);
+            result.putExtras(bundle);
+            return result;
+        }
+    };
+
+
+    @Test
+    public void testTextViewExists(){
+        ViewGroupsActivity activity = rule.getActivity();
+
+        RecyclerView recyclerView = activity.findViewById(R.id.recyclerView);
+
+        assertNotNull(recyclerView);
+
+    }
+
+    @Test
+    public void testTitleBar(){
+        ViewGroupsActivity activity = rule.getActivity();
+
+        String title = activity.getSupportActionBar().getTitle().toString();
+
+        assertEquals(title, "Groups");
+
+    }
+
+}
