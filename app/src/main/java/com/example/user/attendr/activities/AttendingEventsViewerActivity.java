f7ee2f369b2da54ac1e5b1da7f9b67ac2fb3e7c1
Fanner487 <eamontang1@hotmail.com>
2018-04-05 14:16:37 +0100

    Removed action bar

--- app/src/main/java/com/example/user/attendr/activities/AttendingEventsViewerActivity.java
+++ app/src/main/java/com/example/user/attendr/activities/AttendingEventsViewerActivity.java
@@ -81,28 +81,28 @@ public class AttendingEventsViewerActivity extends AppCompatActivity
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_attending_events_viewer, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+////        getMenuInflater().inflate(R.menu.menu_attending_events_viewer, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+////        if (id == R.id.action_settings) {
+////            return true;
+////        }
+//
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 
     // Needed for the fragment inits
     @Override
@@ -144,12 +144,5 @@ public class AttendingEventsViewerActivity extends AppCompatActivity
             }
         });
 
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
     }
 }
--- app/src/main/java/com/example/user/attendr/activities/OrganiseEventsViewerActivity.java
+++ app/src/main/java/com/example/user/attendr/activities/OrganiseEventsViewerActivity.java
@@ -27,20 +27,8 @@ public class OrganiseEventsViewerActivity extends AppCompatActivity
         implements ViewEventsFragment.OnFragmentInteractionListener, ListenerInterface{
 
 
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
-    private SectionsPagerAdapter mSectionsPagerAdapter;
-
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
-    private ViewPager mViewPager;
+    SectionsPagerAdapter mSectionsPagerAdapter;
+    ViewPager mViewPager;
     TabLayout tabLayout;
     FloatingActionMenu fam;
     FloatingActionButton fabCreateEvent, fabCreateGroup;
@@ -80,29 +68,6 @@ public class OrganiseEventsViewerActivity extends AppCompatActivity
 
         setListeners();
 
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_new_event_viewer, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
